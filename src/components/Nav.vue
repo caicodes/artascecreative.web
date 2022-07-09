619de519a335e22990deb964243e223c5d22d660
caicodes <caicodes1@gmail.com>
2022-07-09 03:43:42 -0600

    updated readme with current project notes for push to dev

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,15 @@
 <template>
- <nav>
-    <ul>
-        <li>
-            <router-link to="/">Home</router-link></li>
-            <li><router-link to="/about">About</router-link></li>
-        
-    </ul>
- </nav>
- 
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="/about">About</router-link>
+            </li>
+
+        </ul>
+    </nav>
+
 
 </template>
