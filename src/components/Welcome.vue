6992e8addeb8bb7886a54227bd735e2c683cba94
caicodes <caicodes1@gmail.com>
2022-07-08 20:17:14 -0600

    firebase integration setup complete, data display coming soon

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -35,7 +35,7 @@ const count = ref(0)
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>components/Welcome.vue</code> to test hot module replacement.
   </p>
 </template>
 
